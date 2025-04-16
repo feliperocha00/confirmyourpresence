94d3ec9ca182368adc3a1dc2f9c6f0c2b7869f5d
Felipe Rocha <felipe.pessoal0909@gmail.com>
2025-04-16 20:40:05 -0300

    [IMP] function to verify if the guests exists in the guests.csv file

--- main/models.py
+++ main/models.py
@@ -1,17 +1,28 @@
 from django.db import models
 from django.core.validators import RegexValidator
+import re
 
 class Welcome(models.Model):
-    def find_number(self):
-        return True
-
+    def find_number(self, phone):
+        phone = re.sub(r'\D', '', phone)
+        if Guests.objects.filter(phone=phone).exists():
+            return True
+        else:
+            return False
+    
 class Guests(models.Model):
     name = models.CharField("Nome", max_length=12)
     phone = models.CharField(
         max_length=11,
         validators=[RegexValidator(regex=r'^\d{10,11}$', message='Digite um número de telefone válido.')]
     )
-    presenca = models.CharField(max_length=3, choices=[('sim', 'Sim'), ('nao', 'Não')])
+    parent = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='childs'
+    )
 
 # Create your models here.
 class Confirm(models.Model):
