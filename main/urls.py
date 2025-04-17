29a54d694ff6dddb2d95d877f16336f987256c7a
HQ-FELIPE\ti <felipe.pessoal0909@gmail.com>
2025-04-17 13:26:08 -0300

    [IMP] guests page to read and import the guests list. [TODO][IMP] auth to access the guests and confirmations page

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.welcome, name="welcome"),
     path("not-found/", views.notfound, name="notfound"),
     path("success/", views.success, name="success"),
     path("confirm/", views.confirm, name="confirm_presence"),
-    path("confirmations/", views.confirmations, name="confirmations")
-    # path("home", views.home, name="Home"),
+    path("confirmations/", views.confirmations, name="confirmations"),
+    path("guests/", views.guests, name="guests"),
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
