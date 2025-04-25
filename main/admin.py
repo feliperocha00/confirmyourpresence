6502144c79984f7b7ac5fb91fd65f0ed928b1406
Felipe Rocha <felipe.pessoal0909@gmail.com>
2025-04-25 05:51:09 -0300

    [IMP] allow to manage Guests objects in admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Confirm
+from .models import Confirm, Guests
 # Register your models here.
 
 admin.site.register(Confirm)
+admin.site.register(Guests)
