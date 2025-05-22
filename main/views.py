924e89a9b7af5ed2762a48814f6a23e9184360ed
HQ-FELIPE\ti <felipe.pessoal0909@gmail.com>
2025-05-22 12:09:24 -0300

    [IMP] method post to get confirmations in confirm view

--- main/views.py
+++ main/views.py
@@ -25,11 +25,20 @@ def welcome(request):
     return render(request, "welcome.html")
 
 def confirm(request):
-    if request.method == "POST":
-        form = models.Confirm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("success")
+    if request.method == 'POST':
+        form = models.Guests(request.POST)
+        total_rows = int(request.POST.get('total_rows', 0))
+        items = []
+        for i in range(1, total_rows + 1):
+            name = request.POST.get(f'guest_name_{i}')
+            confirmation = request.POST.get(f'guest_confirmation_{i}')
+
+            guest = form.find_guest(name)
+            if guest:
+                form.save()
+                # items.append({'name': name, 'quantity': confirmation})
+        
+        return render(request, 'success.html', {'items': items})
 
     return render(request, "confirm.html")    
 
@@ -74,7 +83,6 @@ def confirmations(request):
 # @user_passes_test(is_admin)
 def guests(request):
     guest_list = models.Guests.objects.all()
-    print(guest_list)
     return render(request, "guests.html", {"guests": guest_list})
     
 def add_guests(request):
