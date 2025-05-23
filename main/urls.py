2711822e7246752ae01143f3000e154365f1a230
HQ-FELIPE\ti <felipe.pessoal0909@gmail.com>
2025-05-23 08:09:42 -0300

    [IMP] success page derives from confirm view

--- main/urls.py
+++ main/urls.py
@@ -5,8 +5,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("", views.welcome, name="welcome"),
     path("not-found/", views.notfound, name="notfound"),
-    path("success/", views.success, name="success"),
     path("confirm/", views.confirm, name="confirm_presence"),
+    path("confirm/success/", views.success, name="success"),
     path("confirmations/", views.confirmations, name="confirmations"),
     path("guests/", views.guests, name="guests"),
     path("guests/add", views.add_guests, name="add_guests"),
