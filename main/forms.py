8c1d58c3254fba0aaaf413350d380aa6e8203044
HQ-FELIPE\ti <felipe.pessoal0909@gmail.com>
2025-06-09 16:52:49 -0300

    [IMP] product_url field

--- main/forms.py
+++ main/forms.py
@@ -4,7 +4,7 @@ from .models import Gift
 class GiftForm(forms.ModelForm):
     class Meta:
         model = Gift
-        fields = ["name", "price", "image_url"]
+        fields = ["name", "price", "image_url", "product_url"]
         widgets = {
             "name": forms.TextInput(attrs={"class": "form-control"}),
             "price": forms.NumberInput(attrs={"class": "form-control", "step": "0.01"}),
