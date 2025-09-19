b56d2dee608376ffbc4853d2259c42dea00e10b6
HQ-FELIPE\ti <felipe.pessoal0909@gmail.com>
2025-09-19 13:33:49 -0300

    [IMP] set gift taken in ui [ADD] guest form

--- main/forms.py
+++ main/forms.py
@@ -1,13 +1,25 @@
 from django import forms
-from .models import Gift
+from .models import Gift, Guests
 
 class GiftForm(forms.ModelForm):
     class Meta:
         model = Gift
-        fields = ["name", "price", "image_url", "product_url"]
+        fields = ["name", "price", "image_url", "product_url", "taken"]
         widgets = {
             "name": forms.TextInput(attrs={"class": "form-control"}),
             "price": forms.NumberInput(attrs={"class": "form-control", "step": "0.01"}),
             "image_url": forms.URLInput(attrs={"class": "form-control"}),
             "product_url": forms.URLInput(attrs={"class": "form-control"}),
+            "taken": forms.CheckboxInput()
+        }
+
+class GuestsForm(forms.ModelForm):
+    class Meta:
+        model = Guests
+        fields = ["name", "description", "phone", "parent"]
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "description": forms.TextInput(attrs={"class": "form-control"}),
+            "phone": forms.TextInput(attrs={"class": "form-control"}),
+            "parent": forms.Select(attrs={"class": "form-control"}),
         }
